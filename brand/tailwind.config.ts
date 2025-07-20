b2a451dee4e520f34ac437acfdab4fad5f78be5c
Maher Hussain <maherhussain6@gmail.com>
2025-07-20 02:23:39 +0200

    fix tailwind on vercel

--- brand/tailwind.config.ts
+++ brand/tailwind.config.ts
@@ -10,9 +10,7 @@ const {
 const config: Config = {
 	darkMode: ['class'],
 	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
